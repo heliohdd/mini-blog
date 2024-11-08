389b812ee1d7c115c337c76917dcadad7f13142d
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-08 07:55:18 -0300

    Integrando Firebase ao React

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgyvd74rA1_4nG8ehEdoGa5WblsIA2VNk",
+  authDomain: "miniblog-83acc.firebaseapp.com",
+  projectId: "miniblog-83acc",
+  storageBucket: "miniblog-83acc.firebasestorage.app",
+  messagingSenderId: "222875711883",
+  appId: "1:222875711883:web:e4d6a6abfeb49ce66cd78e",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
