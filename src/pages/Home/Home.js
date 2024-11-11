f16883fb6b34a397ce18bed725339fa125217699
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-11 13:30:58 -0300

    Estruturando a pagina Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,45 @@
-import styles from './Home.module.css';
+// CSS
+import styles from "./Home.module.css";
+
+// Hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// Components
 
 const Home = () => {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
-}
+  const [query, setQuery] = useState("");
 
-export default Home
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form} >
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
