e093ff260c13c8b74060eb1007bcee523dd567c9
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-12 18:23:50 -0300

    Criacao da pagina de post individual

---
+++ src/pages/Post/Post.js
@@ -0,0 +1,15 @@
+import styles from "./Post.module.css";
+
+import { useParams } from "react-router-dom";
+
+const Post = () => {
+    const { id } = useParams()
+
+  return (
+    <div>
+        <h1>Post {id}</h1>
+    </div>
+  );
+};
+
+export default Post;
