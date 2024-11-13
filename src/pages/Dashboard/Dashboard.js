5615d57f52dd5b55c4ee1d002fab26765eaeb119
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-13 11:39:40 -0300

    Conclusao da estrutura da pagina Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -11,6 +11,13 @@ const Dashboard = () => {
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {
+    if (loading) {
+      return <p>Carregando...</p>
+    }
+
+  }
+
   return (
     <div>
       <h2>Dashboard</h2>
@@ -23,10 +30,28 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>Tem posts!</div>
-      )}
+        <>
+          <div>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
 
-      {posts && posts.map((post) => <h3>{post.title}</h3>)}
+          {posts && posts.map((post) => (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                  Ver
+                </Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                  Editar
+                </Link>
+                <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
